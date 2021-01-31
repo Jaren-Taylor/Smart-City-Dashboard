8ec217d6e5405e7abcaa3a2cfdc7030773626e6d
caleb <calebwhite@oakland.edu>
2021-01-31 16:41:00 -0500

    Refactored Serialization Code
    Refactored the code in SaveGameManager. Removed repeated code and added some calrification.

--- Smart City Dashboard/Assets/Scripts/Serialization/SaveGameManager.cs
+++ Smart City Dashboard/Assets/Scripts/Serialization/SaveGameManager.cs
@@ -9,13 +9,17 @@ using UnityEngine;
 public static class SaveGameManager 
 {
     public static string LoadFromFile = "";
-
+    /// <summary>
+    /// Loads the game from XML
+    /// </summary>
+    /// <param name="gameLocation">Searches this location for the XML file to load the game</param>
+    /// <returns>Returns the loaded map from the file if found, otherwise returns null</returns>
     public static TileGrid LoadGame(string gameLocation)
     {
         TileGrid grid = null;
         using (var fs = new FileStream(gameLocation, FileMode.Open))
         {
-            DataContractSerializer dcs = new DataContractSerializer(typeof(TileGrid), new Type[] { typeof(RoadTile), typeof(BuildingTile), typeof(Tile) });
+            DataContractSerializer dcs = GetDataContract();
             XmlDictionaryReader reader = XmlDictionaryReader.CreateTextReader(fs, new XmlDictionaryReaderQuotas());
 
             grid = (TileGrid)dcs.ReadObject(reader);
@@ -23,18 +27,28 @@ public static class SaveGameManager
 
         return grid;
     }
-
+    /// <summary>
+    /// Saves the game to XML
+    /// </summary>
+    /// <param name="gameLocation"></param>
+    /// <param name="saveData"></param>
+    /// <returns></returns>
     public static bool SaveGame(string gameLocation, TileGrid saveData)
     {
-        var serializer = new DataContractSerializer(typeof(TileGrid), new Type[] { typeof(RoadTile), typeof(BuildingTile), typeof(Tile) } );
         using (FileStream fs = new FileStream(gameLocation, FileMode.Truncate))
         {
             using (XmlWriter xmlWriter = XmlWriter.Create(fs))
             {
-                serializer.WriteObject(xmlWriter, saveData);
+                var dcs = GetDataContract();
+                dcs.WriteObject(xmlWriter, saveData);
             }
         }
 
         return true;
     }
+    private static DataContractSerializer GetDataContract()
+    {
+        return new DataContractSerializer(typeof(TileGrid), new Type[] { typeof(RoadTile), typeof(BuildingTile), typeof(Tile) });
+    }
+  
 }
