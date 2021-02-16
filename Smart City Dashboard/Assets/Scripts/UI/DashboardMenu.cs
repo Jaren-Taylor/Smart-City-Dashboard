8e51c91f542c7dc0880b9df981316489a2022ca9
Kenny Mead <kennethmead784@gmail.com>
2021-02-16 17:55:54 -0500

    UI UML diagrams

--- Smart City Dashboard/Assets/Scripts/UI/DashboardMenu.cs
+++ Smart City Dashboard/Assets/Scripts/UI/DashboardMenu.cs
@@ -37,7 +37,6 @@ public class DashboardMenu : Menu
 
     public void UpdateSpriteFromTexture(Texture2D texture)
     {
-
         heatmapImage.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 1));
     }
 }
