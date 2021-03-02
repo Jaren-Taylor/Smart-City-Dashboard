215a2889c4d1306874a2635163c1522085b20dc7
a5fuoco <56035615+a5fuoco@users.noreply.github.com>
2021-03-01 20:18:01 -0500

    Added lots of car colors

--- Smart City Dashboard/Assets/Scripts/Entities/VehicleEntity.cs
+++ Smart City Dashboard/Assets/Scripts/Entities/VehicleEntity.cs
@@ -21,13 +21,31 @@ public class VehicleEntity : Entity
 
     public static readonly Dictionary<VehicleColor, string> ColorLookup = new Dictionary<VehicleColor, string>()
     {
-        {VehicleColor.Red, "Materials/Car_Red" },
-        {VehicleColor.Blue, "Materials/Car_Blue" },
+        {VehicleColor.Red, "Materials/CarColors/Car_Red" },
+        {VehicleColor.Blue, "Materials/CarColors/Car_Blue" },
+        {VehicleColor.Black, "Materials/CarColors/Car_Black" },
+        {VehicleColor.White, "Materials/CarColors/Car_White" },
+        {VehicleColor.Gray, "Materials/CarColors/Car_Gray" },
+        {VehicleColor.Silver, "Materials/CarColors/Car_Silver" },
+        {VehicleColor.Brown, "Materials/CarColors/Car_Brown" },
+        {VehicleColor.Gold, "Materials/CarColors/Car_Gold" },
+        {VehicleColor.Green, "Materials/CarColors/Car_Green" },
+        {VehicleColor.Turquoise, "Materials/CarColors/Car_Turquoise" },
+        {VehicleColor.Orange, "Materials/CarColors/Car_Orange" },
     };
 
     public enum VehicleColor {
         Red,
-        Blue
+        Blue,
+        Black,
+        White,
+        Gray,
+        Silver,
+        Brown,
+        Gold,
+        Green,
+        Turquoise,
+        Orange
     }
 
     public static VehicleEntity Spawn(Vector2Int tilePosition, VehicleType type)
