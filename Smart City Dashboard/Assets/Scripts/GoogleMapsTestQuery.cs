f2ad74f1f2befdd9864a35c3a8272e65827c4b55
Jaren Taylor <Jetaylor2@Oakland.edu>
2021-03-20 00:43:00 -0400

    fixing visible creds

--- Smart City Dashboard/Assets/Scripts/GoogleMapsTestQuery.cs
+++ Smart City Dashboard/Assets/Scripts/GoogleMapsTestQuery.cs
@@ -7,44 +7,51 @@ using System.Net;
 using System.IO;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using NUnit.Framework;
 
 public class GoogleMapsTestQuery : MonoBehaviour
 {
     static readonly HttpClient Http = new HttpClient();
     public Texture2D texture;
     public Image picture;
+    private string apikey;
+    private bool KeyCreated = false; 
 
-    private void Start()
+    private void Update()
     {
-        StartCoroutine(GetTexture(CreateQuery()));
+        //crude quickfix
+        if(KeyCreated) StartCoroutine(GetTexture(CreateQuery(apikey)));
     }
 
     /// <summary>
-    /// Given a NxN size, an integer based zoom and a location it will return a Google Maps Static Map in the form of a texture
+    /// Given a NxN size, an integer based zoom, a location, and an apikey it will return a Google Maps Static Map in the form of a texture
     /// </summary>
     /// <param name="size"></param>
     /// <param name="zoom"></param>
     /// <param name="center"></param>
     /// <returns></returns>
-    static string CreateQuery(string size, string zoom, string center)
+    static string CreateQuery(string size, string zoom, string center, string apikey)
     {
 
         center = center.Replace(" ", "+").Replace(",", "+");
-        return $"https://maps.googleapis.com/maps/api/staticmap?size={size}&zoom={zoom}&center={center}&style=feature:landscape|color:0x000000&style=element:labels|invert_lightness:true&style=feature:road|color:0x880000&style=feature:road.local|geometry|color:0x008800&style=feature:road|weight:1&style=feature:road|element:labels|visibility:off&style=feature:poi|element:labels|visibility:off&style=feature:poi|geometry|color:0x880000&style=feature:transit|visibility:off&style=feature:water|visibility:off&style=feature:administrative|visibility:off&key=AIzaSyCb0oNZi6NoC3_sBzA2lHzhNdpCJeGJb2c";
+        return $"https://maps.googleapis.com/maps/api/staticmap?size={size}&zoom={zoom}&center={center}&style=feature:landscape|color:0x000000&style=element:labels|invert_lightness:true&style=feature:road|color:0x880000&style=feature:road.local|geometry|color:0x008800&style=feature:road|weight:1&style=feature:road|element:labels|visibility:off&style=feature:poi|element:labels|visibility:off&style=feature:poi|geometry|color:0x880000&style=feature:transit|visibility:off&style=feature:water|visibility:off&style=feature:administrative|visibility:off&key={apikey}";
     }
 
     /// <summary>
     /// returns a basic static map of saint clair shores for testing purposes
     /// </summary>
     /// <returns></returns>
-    static string CreateQuery()
+    static string CreateQuery(string apikey)
     {
-        return "https://maps.googleapis.com/maps/api/staticmap?size=460x460&zoom=16&center=Saint+Clair+Shores+Michigan&style=feature:landscape|color:0x000000&style=element:labels|invert_lightness:true&style=feature:road|color:0x880000&style=feature:road.local|geometry|color:0x008800&style=feature:road|weight:1&style=feature:road|element:labels|visibility:off&style=feature:poi|element:labels|visibility:off&style=feature:poi|geometry|color:0x880000&style=feature:transit|visibility:off&style=feature:water|visibility:off&style=feature:administrative|visibility:off&key=AIzaSyCb0oNZi6NoC3_sBzA2lHzhNdpCJeGJb2c";
+        Debug.Log($"https://maps.googleapis.com/maps/api/staticmap?size=460x460&zoom=16&center=Saint+Clair+Shores+Michigan&style=feature:landscape|color:0x000000&style=element:labels|invert_lightness:true&style=feature:road|color:0x880000&style=feature:road.local|geometry|color:0x008800&style=feature:road|weight:1&style=feature:road|element:labels|visibility:off&style=feature:poi|element:labels|visibility:off&style=feature:poi|geometry|color:0x880000&style=feature:transit|visibility:off&style=feature:water|visibility:off&style=feature:administrative|visibility:off&key={apikey}"
+    );
+        return $"https://maps.googleapis.com/maps/api/staticmap?size=460x460&zoom=16&center=Saint+Clair+Shores+Michigan&style=feature:landscape|color:0x000000&style=element:labels|invert_lightness:true&style=feature:road|color:0x880000&style=feature:road.local|geometry|color:0x008800&style=feature:road|weight:1&style=feature:road|element:labels|visibility:off&style=feature:poi|element:labels|visibility:off&style=feature:poi|geometry|color:0x880000&style=feature:transit|visibility:off&style=feature:water|visibility:off&style=feature:administrative|visibility:off&key={apikey}";
     }
 
 
     public IEnumerator GetTexture(string uri)
     {
+        KeyCreated = false;
         UnityWebRequest www = UnityWebRequestTexture.GetTexture(uri);
         yield return www.SendWebRequest();
 
@@ -57,6 +64,13 @@ public class GoogleMapsTestQuery : MonoBehaviour
            texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
            picture.sprite = Sprite.Create(texture, new Rect(0,0,texture.width, texture.height) , Vector2.zero);
         }
+    }
+
+    public void SetApiKey(string input)
+    {
+        apikey = input;
+        Debug.Log(apikey);
+        KeyCreated = true;
     }
 
 }
