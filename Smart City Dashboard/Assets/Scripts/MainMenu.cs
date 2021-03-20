be26193459efc90890fc048fc87a13fb60b4977d
Mike <mlabarbera98a@gmail.com>
2021-03-20 10:41:21 -0400

    Imported Lean Tween

--- Smart City Dashboard/Assets/Scripts/MainMenu.cs
+++ Smart City Dashboard/Assets/Scripts/MainMenu.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject Title;
+    [SerializeField]
+    private GameObject[] Buttons;
+
+    public void Start()
+    {
+    }
+
     public void StartGame()
     {
         GameSceneManager.LoadScene(SceneIndexes.BUILD);
