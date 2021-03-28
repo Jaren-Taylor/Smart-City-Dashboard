e0ef49f9f4f798ecfc409a23541bc2a8986ed320
DLidz <46999371+DLidz@users.noreply.github.com>
2021-03-28 15:37:26 -0400

    pooling quick fix

--- Smart City Dashboard/Assets/Scripts/Managers/UIManager.cs
+++ Smart City Dashboard/Assets/Scripts/Managers/UIManager.cs
@@ -64,6 +64,7 @@ public class UIManager : MonoBehaviour
 
     public void LoadMainMenuScene()
     {
+        ObjectPoolerManager.ClearPools();
         GameSceneManager.LoadScene(SceneIndexes.TITLE, "Loading Main Menu");
     }
 
